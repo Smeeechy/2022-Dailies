f47d8d017e50b0957821437fa34fd05e35f81b52
darynsmith <d.maccoy.smith@gmail.com>
2022-01-13 23:34:47 -0700

    implement insert and search methods for a ternary search tree

--- 20220113/Main.java
+++ 20220113/Main.java
@@ -2,13 +2,14 @@
 A ternary search tree is a trie-like data structure where each node may have up to three children. 
 Here is an example which represents the words code, cob, be, ax, war, and we.
 
-       c
-    /  |  \
-   b   o   w
- / |   |   |
-a  e   d   a
-|    / |   | \ 
-x   b  e   r  e  
+	      c
+	    / | \
+	   b  o  w
+	 / |  |  |
+	a  e  d  a
+	|   / |  | \ 
+	x  b  e  r  e  
+
 The tree is structured according to the following rules:
 
 left child nodes link to words lexicographically earlier than the parent prefix
@@ -21,59 +22,77 @@ Implement insertion and search functions for a ternary search tree.
 */
 
 class Main {
-    public static void main(String[] args) {
-        Trie trie = new Trie();
-        for (String str : args) {
-            trie.insert(str);
-            System.out.println("inserted " + str);
-        }
-    }
+	public static void main(String[] args) {
+		Trie trie = new Trie();
+		String[] words = new String[] {"code", "cob", "be", "ax", "war", "we"};
+		for (String word : words) {
+			trie.insert(word);
+		}
+
+		for (String s : args) {
+			System.out.println("'" + s + "' is " + (trie.search(s) ? "" : "not ") + "in this trie");
+		}
+	}
 }
 
 class Trie {
-    Node root = null;
+	Node root = null;
 
-    void insert(String word) {
-        if (root == null) {
-            this.root = new Node();
-            this.root.parent = null;
-            this.root.data = word.charAt(0);
-        }
-        insert_recursive(this.root, word);
-    }
+	void insert(String word) {
+		if (root == null) {
+			this.root = new Node();
+			this.root.data = word.charAt(0);
+		}
+		insert_recursive(this.root, word);
+	}
 
-    private void insert_recursive(Node n, String word) {
-        if (word.equals("")) return;
-        char c = word.charAt(0);
-        if (n.data == '\0') {
-            n.data = c;
-        }
-        if (c < n.data) {
-            if (n.l == null) {
-                n.l = new Node();
-            }
-            insert_recursive(n.l, word);
-        } else if (c > n.data) {
-            if (n.r == null) {
-                n.r = new Node();
-            }
-            insert_recursive(n.r, word);
-        } else {
-            if (word.substring(1).equals("")) return;
-            if (n.m == null) {
-                n.m = new Node();
-            }
-            insert_recursive(n.m, word.substring(1));
-        }
-    }
+	private void insert_recursive(Node n, String word) {
+		if (word.equals("")) return;
+		char c = word.charAt(0);
+		if (n.data == '\0') {
+			n.data = c;
+		}
+		if (c < n.data) {
+			if (n.l == null) {
+				n.l = new Node();
+			}
+			insert_recursive(n.l, word);
+		} else if (c > n.data) {
+			if (n.r == null) {
+				n.r = new Node();
+			}
+			insert_recursive(n.r, word);
+		} else {
+			if (word.substring(1).equals("")) return;
+			if (n.m == null) {
+				n.m = new Node();
+			}
+			insert_recursive(n.m, word.substring(1));
+		}
+	}
 
-    boolean search(String s) {
-        // implement
-        return false;
-    }
+	boolean search(String s) {
+		return search_recursive(this.root, s);
+	}
+
+	private boolean search_recursive(Node n, String s) {
+		if (s.equals("")) return true;
+		char c = s.charAt(0);
+		if (c < n.data) {
+			if (n.l == null) return false;
+			else return search_recursive(n.l, s);
+		} else if (c > n.data) {
+			if (n.r == null) return false;
+			else return search_recursive(n.r, s);
+		} else {
+			if (s.substring(1).equals("")) return true;
+			if (n.m == null) return false;
+			else return search_recursive(n.m, s.substring(1));
+		}
+	}
 }
 
 class Node {
-    Node parent, l, m, r;
-    char data;
+	Node l, m, r;
+	char data;
 }
