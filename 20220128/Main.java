e00e1deeedd1844fb06ee140f01009e0144e50e2
darynsmith <d.maccoy.smith@gmail.com>
2022-01-29 22:54:57 -0700

    given an array of piles of stones, find the biggest pyramid that can be made by removing the fewest stones from each pile

--- 20220128/Main.java
+++ 20220128/Main.java
@@ -9,7 +9,10 @@ determine the lowest cost method to produce this pyramid.
 For example, given the stones [1, 1, 3, 3, 2, 1], the optimal solution is to pay 2 to create [0, 1, 2, 3, 2, 1].
 */
 
+// this is already over-engineered to all hell and i'm bored with it so this is probably the best it's gonna get
+
 import java.util.Arrays;
+import java.util.Collections;
 
 class Main {
 	static int[] stones;
@@ -17,16 +20,40 @@ class Main {
 	public static void main(String[] args) {
 		stones = Arrays.stream(args).mapToInt(Integer::parseInt).toArray();
 
-		int leftIndex = leftMax(0);
-		int leftEndIndex = leftCheck(leftIndex);
-		System.out.println(leftEndIndex);
+		int leftStartIndex = leftMax(0);
+		int leftEndIndex = leftCheck(leftStartIndex);
+/*		System.out.println("left triangle starts at index " + leftStartIndex + " and ends at index " + leftEndIndex + ":");
+		for (int i = leftStartIndex; i <= leftEndIndex; i++) {
+			System.out.print(stones[i] + " ");
+		} */
 
-		// 1 find index of longest left side using leftMax()
-		// 2 repeat until leftCheck() succeeds
-		// 3 repeat for right side
-		// 4 repeat until left and right intersect
-		// 5 intersection of the two should be max area
-		
+		int rightEndIndex = rightMax(stones.length - 1);
+		int rightStartIndex = rightCheck(rightEndIndex);
+/*              System.out.println("right triangle starts at index " + rightStartIndex + " and ends at index " + rightEndIndex + ":");
+                for (int i = rightStartIndex; i <= rightEndIndex; i++) {
+                        System.out.print(stones[i] + " ");
+                } */
+
+		int startIndex, endIndex;
+		if (leftStartIndex > rightStartIndex) startIndex = leftStartIndex;
+		else startIndex = rightStartIndex;
+		if (leftEndIndex < rightEndIndex) endIndex = leftEndIndex;
+		else endIndex = rightEndIndex;
+
+		System.out.println("triangle starts at index " + startIndex + " and ends at index " + endIndex + ":");
+
+		int max = Arrays.stream(stones).max().getAsInt();
+
+		for (int row = max; row >= 0; row--) {
+			for (int col = 0; col < stones.length; col++) {
+				if (row == 0) System.out.print(col + " ");
+				else if (stones[col] >= row) System.out.print("x ");
+				else System.out.print("  ");
+			}
+			System.out.println();
+		}
+
+		// TODO: implement cost methods and print cost to console
 	}
 
 	public static int leftMax(int startIndex) {
@@ -55,23 +82,14 @@ class Main {
 		boolean valid = true;
 		int curValue = 1;
 		int curIndex = index;
-		System.out.println("increasing from " + curIndex);
-		while (stones[++curIndex] > curValue++) {
-			System.out.print("curIndex=" + curIndex + " ");
-			System.out.println("curValue=" + curValue);
-		}
-		System.out.println("decreasing from " + curIndex);
+		while (stones[++curIndex] > curValue++) {}
 		curValue--;
 		while (curValue-- >= 1 && ++curIndex < stones.length) {
-			System.out.print("curIndex=" + curIndex + " ");
-			System.out.println("curValue=" + curValue);
 			if (curIndex > stones.length - 1) {
-				System.out.println("oob at " + curIndex);
 				valid = false;
 				break;
 			}
 			if (stones[curIndex] < curValue) {
-				System.out.println("too short at " + curIndex);
 				valid = false;
 				break;
 			}
@@ -82,7 +100,21 @@ class Main {
 	}
 
 	public static int rightMax(int startIndex) {
-		return -1;
+                int index = stones.length - 1;
+                int max = 0;
+                for (int i = startIndex; i >= 0; i--) {
+                        int len = 1;
+                        for (int j = i - 1; j >= 0; j--) {
+                                if (stones[j] > len) {
+                                        len++;
+                                } else break;
+                        }
+                        if (len >= max) {
+                                max = len;
+                                index = i;
+                        }
+                }
+                return index;
 	}
 
 	public static int rightCost(int index) {
@@ -90,6 +122,23 @@ class Main {
 	}
 
 	public static int rightCheck(int index) {
-		return -1;		
+		boolean valid = true;
+                int curValue = 1;
+                int curIndex = index;
+                while (stones[--curIndex] > curValue++) {}
+                curValue--;
+                while (curValue-- >= 1 && --curIndex >= 0) {
+                        if (curIndex < 0) {
+                                valid = false;
+                                break;
+                        }
+                        if (stones[curIndex] < curValue) {
+                                valid = false;
+                                break;
+                        }
+
+                }
+                if (valid) return ++curIndex;
+                else return index;		
 	}
 }
