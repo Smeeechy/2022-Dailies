3c8f9f98ffd26740e519c9f931f12e7828cb96f6
Daryn Smith <d.maccoy.smith@gmail.com>
2022-01-31 14:33:23 -0700

    a much more fleshed out version of the bellman-ford algorithm. will potentially use for detecting currency arbitrage

--- 20220130/Main.java
+++ 20220130/Main.java
@@ -10,51 +10,70 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 class Main {
+	static int[] costs;
 	static ArrayList<Vertex> graph;
 
 	static {
 		graph = new ArrayList<>();
-		Vertex zero = new Vertex(0);
-		Vertex one = new Vertex(1);
-		Vertex two = new Vertex(2);
-		Vertex three = new Vertex(3);
-		Vertex four = new Vertex(4);
-		Vertex five = new Vertex(5);
-		zero.addEdge(new Edge(one, 10));
-		zero.addEdge(new Edge(five, 8));
-		one.addEdge(new Edge(three, 2));
-		two.addEdge(new Edge(one, 1));
-		three.addEdge(new Edge(two, -2));
-		four.addEdge(new Edge(one, -4));
-		four.addEdge(new Edge(three, -1));
-		five.addEdge(new Edge(four, 1));
-		graph.add(zero);
-		graph.add(one);
-		graph.add(two);
-		graph.add(three);
-		graph.add(four);
-		graph.add(five);
+		Vertex v0 = new Vertex(0);
+		Vertex v1 = new Vertex(1);
+		Vertex v2 = new Vertex(2);
+		Vertex v3 = new Vertex(3);
+		Vertex v4 = new Vertex(4);
+		Vertex v5 = new Vertex(5);
+		Vertex v6 = new Vertex(6);
+		Vertex v7 = new Vertex(7);
+		Vertex v8 = new Vertex(8);
+		Vertex v9 = new Vertex(9);
+
+		v0.addCostToVertex(5, v1);
+		v1.addCostToVertex(20, v2);
+		v1.addCostToVertex(30, v5);
+		v1.addCostToVertex(60, v6);
+		v2.addCostToVertex(10, v3);
+		v2.addCostToVertex(75, v4);
+		v3.addCostToVertex(-15, v2);
+		v4.addCostToVertex(100, v9);
+		v5.addCostToVertex(25, v4);
+		v5.addCostToVertex(5, v6);
+		v5.addCostToVertex(50, v8);
+		v6.addCostToVertex(-50, v7);
+		v7.addCostToVertex(-10, v8);
+
+		graph.add(v0);
+		graph.add(v1);
+		graph.add(v2);
+		graph.add(v3);
+		graph.add(v4);
+		graph.add(v5);
+		graph.add(v6);
+		graph.add(v7);
+		graph.add(v8);
+		graph.add(v9);
 
 		for (Vertex v : graph) {
 			for (Edge e : v.edges) {
 				System.out.println(v.id + " -> " + e.target.id + ": " + e.cost);
 			}
-			System.out.println("-----");
 		}
 	}
 
 	public static void main(String[] args) {
-		int[] costs = new int[graph.size()];
+		costs = new int[graph.size()];
 		for (int i = 0; i < costs.length; i++) {
 			costs[i] = Integer.MAX_VALUE;
 		}
 		costs[0] = 0;
 
-		System.out.println("start: " + Arrays.toString(costs));
-		for (int i = 0; i < costs.length; i++) {
+		for (int i = 0; i < costs.length - 1; i++) {
 			if (!relaxEdges(costs)) break;
-			System.out.println("iteration " + i + ": " + Arrays.toString(costs));
 		}
+
+		for (int i = 0; i < costs.length - 1; i++) {
+			findLoops(costs);
+		}
+
+		print();
 	}
 
 	public static boolean relaxEdges(int[] costs) {
@@ -70,6 +89,29 @@ class Main {
 		}
 		return improved;
 	}
+
+	public static void findLoops(int[] costs) {
+		for (Vertex v : graph) {
+			for (Edge e : v.edges) {
+				if (costs[v.id] == Integer.MIN_VALUE) costs[e.target.id] = Integer.MIN_VALUE;
+				if (costs[e.target.id] > costs[v.id] + e.cost) costs[v.id] = Integer.MIN_VALUE;
+			}
+		}
+	}
+
+	public static void print() {
+		System.out.print("[");
+		for (int i = 0; i < costs.length; i++) {
+			int cost = costs[i];
+			String cost_str = "";
+			if (cost == Integer.MAX_VALUE) cost_str += "\u221E";
+			else if (cost == Integer.MIN_VALUE) cost_str += "-\u221E";
+			else cost_str += cost;
+			if (i < costs.length - 1) cost_str += ", ";
+			System.out.print(cost_str);
+		}
+		System.out.println("]");
+	}
 }
 
 class Vertex {
@@ -80,8 +122,8 @@ class Vertex {
 		this.id = id;
 	}
 
-	void addEdge(Edge edge) {
-		this.edges.add(edge);
+	void addCostToVertex(int cost, Vertex target) {
+		this.edges.add(new Edge(target, cost));
 	}
 }
 
