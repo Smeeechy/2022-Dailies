b7a00ea50ccf5be57a200b1f5dc17a7d940c35d4
Daryn Smith <d.maccoy.smith@gmail.com>
2022-03-13 21:26:29 -0700

    implement a method for trinary reduction

--- 20220313/Main.java
+++ 20220313/Main.java
@@ -16,16 +16,47 @@ For example, given the input ['R', 'G', 'B', 'G', 'B'], it is possible to end up
 ['R']                     |
 */
 
+import java.util.Arrays;
+import java.util.ArrayList;
+
 class Main {
 	public static void main(String[] args) {
-		
+		// standardize the inputs
+		String[] quxes = Arrays.stream(args).map(s -> s.toUpperCase()).toArray(String[]::new);
+
+		// all changes should be finished after the same number of iterations as the length of the original input list
+		for (int iteration = 0; iteration < args.length; iteration++) {
+			// print out the current list, stop evaluating if down to last qux
+			System.out.println(Arrays.toString(quxes));
+			if (quxes.length == 1) break;
+
+			// don't know # of combinations beforehand so result must be an arraylist rather than an array
+			ArrayList<String> newQuxes = new ArrayList<>();
+
+			// iterate through all quxes and combine where possible
+			for (int i = 0; i < quxes.length; i++) {
+				int j;
+				if (i == quxes.length - 1) j = i;  // added to evaluate last qux and also not throw exception
+				else j = i + 1;
+
+				// post incrementing i here because we want to skip j as it shouldn't exist anymore
+				if (!quxes[i].equals(quxes[j])) newQuxes.add(combine(quxes[i++], quxes[j]));
+				else newQuxes.add(quxes[i]);
+			}
+
+			// prepare array for next iteration
+			quxes = newQuxes.toArray(String[]::new);
+		}
 	}
-}
 
-class Qux {
-	String color;
-
-	Qux(String color) {
-		this.color = color;
+	// functional but inelegant and unscalable
+	static String combine(String first, String second) {
+		if (first.equals("R") && second.equals("G")) return "B";
+		if (first.equals("R") && second.equals("B")) return "G";
+		if (first.equals("G") && second.equals("R")) return "B";
+		if (first.equals("G") && second.equals("B")) return "R";
+		if (first.equals("B") && second.equals("R")) return "G";
+		if (first.equals("B") && second.equals("G")) return "R";
+		return "borke";
 	}
 }
