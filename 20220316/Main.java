c9bd85d5ae207985d3d4cc5c00657283aa60b952
Daryn Smith <d.maccoy.smith@gmail.com>
2022-03-16 23:27:49 -0600

    started but not done yet

---
+++ 20220316/Main.java
@@ -0,0 +1,31 @@
+/*
+Given a string s and a list of words words, where each word is the same length, find all starting indices of substrings 
+in s that is a concatenation of every word in words exactly once.
+
+For example, given s = "dogcatcatcodecatdog" and words = ["cat", "dog"], return [0, 13], since "dogcat" starts at index 0 
+and "catdog" starts at index 13.
+
+Given s = "barfoobazbitbyte" and words = ["dog", "cat"], return [] since there are no substrings composed of "dog" and 
+"cat" in s.
+
+The order of the indices does not matter.
+*/
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+class Main {
+	public static void main(String[] args) {
+		String s = args[args.length - 1];
+		String[] words = new String[args.length - 1];
+		for (int i = 0; i < words.length; i++) {
+			words[i] = args[i];
+		}
+		System.out.println(s);
+		System.out.println(Arrays.toString(words));
+	}
+
+	public static int[] findConcats(String[] words, String s) {
+		return new int[1];
+	}
+}
