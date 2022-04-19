dcb79580989a25299f01f5fd4d0cd07153595a35
Daryn Smith <d.maccoy.smith@gmail.com>
2022-04-18 21:13:39 -0600

    given a list of integers representing people's weights and a weight limit k, find the minimum number of boats required to carry every person to safety. boats can only carry up to two people

---
+++ 20220418/dcp.js
@@ -0,0 +1,32 @@
+/*
+An imminent hurricane threatens the coastal town of Codeville. If at most two people can fit in a rescue boat, and the 
+maximum weight limit for a given boat is k, determine how many boats will be needed to save everyone.
+
+For example, given a population with weights [100, 200, 150, 80] and a boat limit of 200, the smallest number of boats 
+required will be three.
+*/
+
+const args = process.argv.slice(2)
+const k = parseInt(args[0])
+const popWeights = args.slice(1).map(Number).sort((a, b) => a - b)
+
+let total = 0
+let left = 0
+let right = popWeights.length - 1
+
+while (left < right) {
+    console.log(left, right)
+    let l = popWeights[right]
+    let r = popWeights[left]
+    console.log(l, r)
+    if (r > (k - l)) {
+        total += 1
+        right -= 1
+    } else {
+        total += 1
+        left += 1
+        right -= 1
+    }
+}
+
+console.log(`It would take ${total} boats to save everyone with those weights`)
