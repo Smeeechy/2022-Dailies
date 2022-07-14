56f60513c4653b4806ea6bbe08e24eaa8a0896a0
Daryn Smith <d.maccoy.smith@gmail.com>
2022-07-13 22:03:16 -0600

    merge overlapping intervals

---
+++ javascript/20220713/dcp.js
@@ -0,0 +1,23 @@
+/*
+
+*/
+
+const mergeOverlappingIntervals = array => {
+  array = array.sort((a, b) => a[0] - b[0])
+  while (true) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+      if (i !== array.length - 1 && array[i][1] >= array[i + 1][0]) {
+        newArray.push([array[i][0], Math.max(array[i][1], array[i + 1][1])])
+        i++
+      } else newArray.push(array[i])
+    }
+    if (array.length === newArray.length) return newArray
+    else array = newArray
+  }
+}
+
+const args = process.argv.slice(2)
+const intervals = JSON.parse(args[0])
+const result = mergeOverlappingIntervals(intervals)
+console.log(result)
