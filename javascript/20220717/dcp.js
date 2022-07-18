3f9837060706e8375c547ffa6739c94e0b60d2bf
Daryn Smith <d.maccoy.smith@gmail.com>
2022-07-18 00:45:29 -0600

    min number of coins for exact change

--- javascript/20220717/dcp.js
+++ javascript/20220717/dcp.js
@@ -4,19 +4,18 @@ Find the minimum number of coins needed to make exact change n using only the gi
 
 const minNumberOfCoinsForChange = (n, denoms) => {
   const coins = denoms.sort((a, b) => a - b)
-  let best = [0]
+  let best = new Array(n + 1)
+  best.fill(-1)
+  best[0] = 0
   for (let coin of coins) {
-    for (let target = 0; target <= n; target++) {
-      if (coin <= target) {
-        const diff = target - coin
-        best[target] = 1 + (best[diff] ?? Infinity)
-      } else {
-        if (isNaN(best[target])) best[target] = Infinity
+    for (let target = coin; target <= n; target++) {
+      const diff = target - coin
+      if (diff >= 0 && best[diff] >= 0) {
+        if (best[target] === -1 || 1 + best[diff] < best[target]) best[target] = 1 + best[diff]
       }
     }
   }
-  if (best[n - 1] === Infinity) return -1
-  else return best[n - 1]
+  return best[n]
 }
 
 const args = process.argv.slice(2)
