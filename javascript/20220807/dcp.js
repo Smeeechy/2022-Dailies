572b6b68c3e0be2c9ce6e4405cee59b792f580af
Daryn Smith <d.maccoy.smith@gmail.com>
2022-08-07 18:58:00 -0600

    remove islands

---
+++ javascript/20220807/dcp.js
@@ -0,0 +1,76 @@
+/*
+Given a matrix of ones and zeros, with ones representing land and zeros representing water, remove all islands from the matrix in-place.
+An "island" is defined as a group of non-diagonally contiguous ones that do not touch the outer border of the matrix.
+*/
+
+const removeIslands = matrix => {
+  const islands = findIslands(matrix)
+  for (let island of islands) {
+    for (let [row, col] of island) {
+      matrix[row][col] = 0
+    }
+  }
+  return matrix
+}
+
+// return an array of arrays of coordinates for each island
+const findIslands = matrix => {
+  const islands = []
+
+  // matrix of booleans for whether or not a cell has already been checked
+  const visited = new Array(matrix.length)
+    .fill()
+    .map(row => new Array(matrix[0].length).fill(false))
+
+  for (let row = 1; row < matrix.length - 1; row++) {
+    for (let col = 1; col < matrix[0].length - 1; col++) {
+      if (matrix[row][col] === 1 && !visited[row][col]) {
+        visited[row][col] = true
+        let valid = true
+        const island = [[row, col]]
+        const toCheck = [...neighbors(matrix, row, col)]
+        while (toCheck.length > 0) {
+          const [cRow, cCol] = toCheck.pop()
+          const newNeighbors = neighbors(matrix, cRow, cCol)
+            .filter(([nRow, nCol]) => !visited[nRow][nCol])
+          toCheck.push(...newNeighbors)
+          if (isBoundary(matrix, cRow, cCol)) valid = false
+          visited[cRow][cCol] = true
+          island.push([cRow, cCol])
+        }
+        if (valid) islands.push(island)
+      }
+    }
+  }
+  return islands
+}
+
+// returns an array of neighboring 1s
+const neighbors = (matrix, row, col) => {
+  return [
+    [row + 1, col],
+    [row - 1, col],
+    [row, col + 1],
+    [row, col - 1]
+  ].filter(neighbor =>
+    neighbor[0] >= 0 &&
+    neighbor[0] < matrix.length &&
+    neighbor[1] >= 0 &&
+    neighbor[1] < matrix[0].length
+  ).filter(neighbor => matrix[neighbor[0]][neighbor[1]] === 1)
+}
+
+// returns whether or not the given cell is on the matrix boundary
+const isBoundary = (matrix, row, col) => {
+  return (
+    row === 0 ||
+    row === matrix.length - 1 ||
+    col === 0 ||
+    col === matrix[0].length - 1
+  )
+}
+
+const args = process.argv.slice(2)
+const matrix = JSON.parse(args[0])
+removeIslands(matrix)
+console.log(matrix)
