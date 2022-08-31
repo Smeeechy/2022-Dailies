328db1e10b4742658d6f2e7ac546303ac658e07c
Daryn Smith <d.maccoy.smith@gmail.com>
2022-08-30 23:41:53 -0600

    calendar matching

--- javascript/20220830/dcp.js
+++ javascript/20220830/dcp.js
@@ -1,10 +1,9 @@
 /*
-Given a list each of yours and a coworker's meeting times for a day, each
-of your earliest and latest avail times, and a meeting duration, return a
-list of all valid potential meeting times.
+Given a list each of yours and a coworker's meeting times for a day, each of your earliest and latest 
+availability times, and a meeting duration, return a list of all valid open timeblocks for the meeting.
 */
 
-// helper functions for converting time strings to ints and vice versa
+// helper methods for converting time strings to ints and vice versa
 String.prototype.toMins = function () {
   const [hours, minutes] = this.split(':').map(Number)
   return hours * 60 + minutes
@@ -16,76 +15,75 @@ Number.prototype.toTime = function () {
   return hours + ':' + minutes
 }
 
-const calendarMatching = (calOne, availOne, calTwo, availTwo, duration) => {
-  // prepping data - converting time strings to ints for total minutes
-  calOne = calOne
-    .map(appt => appt.map(time => time.toMins()))
-    .sort((a, b) => a[0] - b[0])
-  calTwo = calTwo
-    .map(appt => appt.map(time => time.toMins()))
-    .sort((a, b) => a[0] - b[0])
-  const avail = [
-    Math.max(availOne[0].toMins(), availTwo[0].toMins()),
-    Math.min(availOne[1].toMins(), availTwo[1].toMins())
+const calendarMatching = (calendarOne, availabilityOne, calendarTwo, availabilityTwo, duration) => {
+  // prepping data
+  // converting time strings to ints for total minutes
+  calendarOne = calendarOne.map(appt => appt.map(time => time.toMins()))
+  calendarTwo = calendarTwo.map(appt => appt.map(time => time.toMins()))
+  // sorting and merging the two calendars
+  const unmergedCalendar = [...calendarOne, ...calendarTwo].sort((a, b) => a[0] - b[0])
+  const calendar = merge(unmergedCalendar)
+  // merging availability timeframe
+  const [earliestOne, latestOne] = availabilityOne
+  const [earliestTwo, latestTwo] = availabilityTwo
+  const availability = [
+    Math.max(earliestOne.toMins(), earliestTwo.toMins()),
+    Math.min(latestOne.toMins(), latestTwo.toMins())
   ]
 
-  // accounting for zero/negative avail overlap
-  if (avail[0] >= avail[1] || avail[1] - avail[0] < duration) return []
+  // accounting for zero/negative joint availability overlap
+  const [earliest, latest] = availability
+  if (earliest >= latest || latest - earliest < duration) return []
 
-  // brute force check every 15 mins between joint availability frame
-  const meetings = []
-  for (let start = avail[0]; start <= avail[1] + duration; start += 15) {
+  // brute force check every 15 mins between joint availability timeframe
+  const validMeetingTimes = []
+  for (let start = earliest; start <= latest + duration; start += 15) {
     const meeting = [start, start + duration]
-    if (isValidMeeting(calOne, calTwo, avail, meeting)) meetings.push(meeting)
+    if (isValidMeetingTime(calendar, availability, meeting)) validMeetingTimes.push(meeting)
   }
 
   // condense valid meetings into list of valid time blocks
-  const timeBlocks = []
-  let timeBlock = []
-  for (const [meetingStart, meetingEnd] of meetings) {
-    if (timeBlock.length === 0) timeBlock = [meetingStart, meetingEnd]
-    else {
-      const [blockStart, blockEnd] = timeBlock
-      if (meetingStart > blockEnd) {
-        timeBlocks.push(timeBlock)
-        timeBlock = [meetingStart, meetingEnd]
-      } else if (meetingStart <= blockEnd && meetingEnd > blockEnd) {
-        timeBlock = [blockStart, meetingEnd]
-      }
-    }
-  }
-  if (timeBlock.length !== 0) timeBlocks.push(timeBlock)
+  const timeBlocks = merge(validMeetingTimes)
 
   // reformatting and returning valid meeting blocks
-  return timeBlocks.map(([blockStart, blockEnd]) => [
-    blockStart.toTime(),
-    blockEnd.toTime()
-  ])
+  return timeBlocks.map(timeBlock => timeBlock.map(minutes => minutes.toTime()))
 }
 
 // meeting is valid if:
-// 1. it is between the joint availability timeframe
-// 2. it doesn't overlap with any appointments from either calendar
-const isValidMeeting = (calOne, calTwo, avail, meeting) => {
-  const [earliestAvail, latestAvail] = avail
+// 1. it is inside the joint availability timeframe
+// 2. it doesn't overlap with any appointments from calendar
+const isValidMeetingTime = (calendar, availability, meeting) => {
+  const [earliest, latest] = availability
   const [meetingStart, meetingEnd] = meeting
 
   // check if meeting is outside joint availability timeframe
-  if (meetingStart < earliestAvail || meetingEnd > latestAvail) return false
+  if (meetingStart < earliest || meetingEnd > latest) return false
 
-  // check against first calendar appointments
-  for (const [apptStart, apptEnd] of calOne) {
-    if (meetingStart >= apptStart && meetingStart < apptEnd) return false
-    if (meetingEnd > apptStart && meetingEnd <= apptEnd) return false
-  }
-
-  // check against second calendar appointments
-  for (const [apptStart, apptEnd] of calTwo) {
+  // check against calendar appointments
+  for (const [apptStart, apptEnd] of calendar) {
     if (meetingStart >= apptStart && meetingStart < apptEnd) return false
     if (meetingEnd > apptStart && meetingEnd <= apptEnd) return false
   }
 
   return true
+}
+
+// helper method for merging overlapping timeframes
+const merge = array => {
+  const merged = []
+  let block = []
+  for (const [start, end] of array) {
+    if (block.length === 0) block = [start, end]
+    else {
+      const [blockStart, blockEnd] = block
+      if (start > blockEnd) {
+        merged.push(block)
+        block = [start, end]
+      } else if (start <= blockEnd && end > blockEnd) block = [blockStart, end]
+    }
+  }
+  if (block.length !== 0) merged.push(block)
+  return merged
 }
 
 const args = process.argv.slice(2)
@@ -94,11 +92,5 @@ const availabilityOne = JSON.parse(args[1])
 const calendarTwo = JSON.parse(args[2])
 const availabilityTwo = JSON.parse(args[3])
 const duration = parseInt(args[4])
-const result = calendarMatching(
-  calendarOne,
-  availabilityOne,
-  calendarTwo,
-  availabilityTwo,
-  duration
-)
+const result = calendarMatching(calendarOne, availabilityOne, calendarTwo, availabilityTwo, duration)
 console.log(result)
