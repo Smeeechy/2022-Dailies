0d9956a5228243a0673a2a4996c8b6d48fe90434
Daryn Smith <d.maccoy.smith@gmail.com>
2022-09-13 22:20:22 -0600

    zig zag traverse

--- javascript/20220913/dcp.js
+++ javascript/20220913/dcp.js
@@ -51,11 +51,6 @@ const zigzagTraverse = matrix => {
 }
 
 const args = process.argv.slice(2)
-const matrix = [
-  [1, 3, 4, 10],
-  [2, 5, 9, 11],
-  [6, 8, 12, 15],
-  [7, 13, 14, 16]
-]
+const matrix = JSON.parse(args[0])
 const result = zigzagTraverse(matrix)
 console.log(result)
