58162d31291c25d07aec2df78d39bf6a0f313e68
Daryn Smith <d.maccoy.smith@gmail.com>
2022-10-14 01:06:10 -0600

    boggle board

--- typescript/20221013/dcp.ts
+++ typescript/20221013/dcp.ts
@@ -1,8 +1,10 @@
 /*
+Given a matrix of characters and a list of words, determine all the words that can be found in the
+matrix by connecting characters in any direction and without using any element more than
+once for a single word.
 
+For more info, look up the rules of Boggle.
 */
-
-import { deflateSync } from 'zlib'
 
 type TrieNode = {
   [key: string]: TrieNode | string
@@ -21,47 +23,49 @@ class SuffixTrie {
   }
 }
 
+// this is a convoluted mess but it's late and i'm tired and, most importantly, it works!
+// so i'm not gonna annotate/refactor it
 export const boggleBoard = (board: string[][], words: string[]): string[] => {
   const trie = new SuffixTrie()
   for (let word of words) trie.add(word)
+  const result: string[] = []
 
-  const dfs = (row: number, col: number, used: number[][] = []): void => {}
-
-  const getNeighbors = (row: number, col: number, used: number[][]): number[][] => {
-    const neighbors: number[][] = []
-    for (let r = row - 1; r <= row + 1; r++) {
-      for (let c = col - 1; c <= col + 1; c++) {
-        if (r === row && c === col) continue
-        for (let [x, y] of used) {
+  const dfs = (row: number, col: number, node: TrieNode, used: number[][] = []): void => {
+    if ('*' in node) result.push(node['*'] as string)
+    const getNeighbors = (): number[][] => {
+      let neighbors: number[][] = []
+      for (let r = row - 1; r <= row + 1; r++) {
+        if (r < 0 || r >= board.length) continue
+        for (let c = col - 1; c <= col + 1; c++) {
+          if (c < 0 || c >= board[row].length) continue
+          if (r === row && c === col) continue
+          neighbors.push([r, c])
         }
       }
+      used.forEach(([usedRow, usedCol]) => {
+        neighbors = neighbors.filter(([r, c]) => r !== usedRow || c !== usedCol)
+      })
+      return neighbors
     }
-    return neighbors
+
+    const neighbors = getNeighbors()
+    for (let [neighborRow, neighborCol] of neighbors) {
+      const char = board[neighborRow][neighborCol]
+      if (char in node) dfs(neighborRow, neighborCol, node[char] as TrieNode, [...used, [row, col]])
+    }
   }
 
-  const result: string[] = []
   for (let row = 0; row < board.length; row++) {
     for (let col = 0; col < board[row].length; col++) {
-      if (board[row][col] in trie.root) {
-        dfs(row, col)
-      }
+      const char = board[row][col]
+      if (char in trie.root) dfs(row, col, trie.root[char] as TrieNode)
     }
   }
-  return result
+  return words.filter(word => result.includes(word))
 }
 
 const args = process.argv.slice(2)
-const board = [
-  ['t', 'h', 'i', 's', 'i', 's', 'a'],
-  ['s', 'i', 'm', 'p', 'l', 'e', 'x'],
-  ['b', 'x', 'x', 'x', 'x', 'e', 'b'],
-  ['x', 'o', 'g', 'g', 'l', 'x', 'o'],
-  ['x', 'x', 'x', 'D', 'T', 'r', 'a'],
-  ['R', 'E', 'P', 'E', 'A', 'd', 'x'],
-  ['x', 'x', 'x', 'x', 'x', 'x', 'x'],
-  ['N', 'O', 'T', 'R', 'E', '-', 'P'],
-  ['x', 'x', 'D', 'E', 'T', 'A', 'E']
-]
-const words = ['this', 'is', 'not', 'a', 'simple', 'boggle', 'board', 'test', 'REPEATED', 'NOTRE-PEATED']
+const board = JSON.parse(args[0])
+const words = args.slice(1)
 const result = boggleBoard(board, words)
 console.log(result)
