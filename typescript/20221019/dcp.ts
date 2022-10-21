414b83f022e886a062f8030467f75c16d1c265f3
Daryn Smith <d.maccoy.smith@gmail.com>
2022-10-21 00:15:16 -0600

    rotate matrix in-place

--- typescript/20221019/dcp.ts
+++ typescript/20221019/dcp.ts
@@ -2,33 +2,15 @@
 Given a matrix, rotate it in-place.
 */
 
-export const rotate = (matrix: number[][]): void => {
+const rotate = (matrix: number[][]): void => {
   for (let i = 0; i <= matrix.length / 2; i++) {
-    let row = i
-    let col = i
-
-    // down
-    while (row < matrix.length - i - 1) {
-      swap(row, col, row + 1, col, matrix)
-      row++
-    }
-
-    // right
-    while (col < matrix[row].length - i - 1) {
-      swap(row, col, row, col + 1, matrix)
-      col++
-    }
-
-    // up
-    while (row > i) {
-      swap(row, col, row - 1, col, matrix)
-      row--
-    }
-
-    // left
-    while (col > i) {
-      swap(row, col, row, col - 1, matrix)
-      col--
+    for (let j = 1; j < matrix[i].length - 2 * i; j++) {
+      let row = i
+      let col = i
+      while (row < matrix.length - i - 1) swap(row, col, ++row, col, matrix)
+      while (col < matrix[row].length - i - 1) swap(row, col, row, ++col, matrix)
+      while (row > i) swap(row, col, --row, col, matrix)
+      while (col > i + 1) swap(row, col, row, --col, matrix)
     }
   }
 }
@@ -40,11 +22,6 @@ const swap = (y1: number, x1: number, y2: number, x2: number, matrix: number[][]
 }
 
 const args = process.argv.slice(2)
-// const matrix = JSON.parse(args[0])
-const matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
-]
+const matrix = JSON.parse(args[0])
 rotate(matrix)
 console.log(matrix)
