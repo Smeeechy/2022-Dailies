34f63768c3667a28f928132d3cc2d30059073592
Daryn Smith <d.maccoy.smith@gmail.com>
2022-10-27 01:24:46 -0600

    remove duplicates in constant space

--- typescript/20221026/dcp.ts
+++ typescript/20221026/dcp.ts
@@ -1,16 +1,26 @@
 /*
-
+Given a sorted array of integers, remove all duplicate values from the array
+and return the length of the new array using constant space.
 */
 
-const removeDuplicates = (nums: number[]): number => {
+export const removeDuplicates = (nums: number[]): number => {
   let current = nums[0]
   for (let i = 1; i < nums.length; i++) {
-    if (nums[i] === current) {
-      nums[i] = -Infinity
-    } else {
+    if (nums[i] === current) nums[i] = -Infinity
+    else current = nums[i]
+  }
+  let slot = 1
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== -Infinity) {
+      nums[slot] = nums[i]
+      if (slot !== i) nums[i] = -Infinity
+      slot++
     }
   }
-  return -1
+  return slot
 }
 
 const args = process.argv.slice(2)
+const nums = args.map(Number)
+nums.length = removeDuplicates(nums)
+console.log(nums)
