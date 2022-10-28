a47610a46f6b50deee8b0652eef0d6de19ce17e5
Daryn Smith <d.maccoy.smith@gmail.com>
2022-10-27 23:58:27 -0600

    implement math.pow

--- typescript/20221027/dcp.ts
+++ typescript/20221027/dcp.ts
@@ -2,10 +2,12 @@
 Implement pow(x, n), which calculates x raised to the power of n.
 */
 
+// opted for recursion instead of the lame (and slow) method of repeated multiplication.
 const myPow = (base: number, exp: number): number => {
   if (exp === -1) return 1 / base
   if (exp === 0) return 1
   if (exp === 1) return base
+  // drastically cuts down computation time. also '~~' is just a quick way to truncate floats
   const halfPow = myPow(base, ~~(exp / 2))
   return myPow(base, exp % 2) * halfPow * halfPow
 }
