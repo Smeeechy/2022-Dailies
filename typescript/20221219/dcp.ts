5d58dd3d262242b295a48d75196dd45f9bc1403c
Daryn Smith <d.maccoy.smith@gmail.com>
2022-12-19 23:19:26 -0700

    fizz buzz

---
+++ typescript/20221219/dcp.ts
@@ -0,0 +1,23 @@
+/*
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+*/
+
+export const fizzBuzz = (n: number): string[] => {
+  return new Array(n).fill('').map((_, index) => {
+    const num = index + 1
+    if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz'
+    if (num % 3 === 0) return 'Fizz'
+    if (num % 5 === 0) return 'Buzz'
+    return num.toString()
+  })
+}
+
+const args = process.argv.slice(2)
+const n = parseInt(args[0])
+const result = fizzBuzz(n)
+console.log(result)
