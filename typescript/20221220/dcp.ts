c5532931219a4ac3072f0dd3b61ce721c432ce3d
Daryn Smith <d.maccoy.smith@gmail.com>
2022-12-21 23:10:51 -0700

    symmetrical binary tree

---
+++ typescript/20221220/dcp.ts
@@ -0,0 +1,53 @@
+/*
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+*/
+
+type TreeNode = {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+}
+
+export const isSymmetric = (root: TreeNode | null): boolean => {
+  if (root === null) return true
+  if (root.left && root.right && root.left.val !== root.right.val) return false
+  const inOrderLeft: number[] = []
+  const inOrderRight: number[] = []
+  inOrderTraversal(root.left, inOrderLeft)
+  inOrderTraversal(root.right, inOrderRight)
+  if (inOrderLeft.length !== inOrderRight.length) return false
+  for (let i = 0; i < inOrderLeft.length; i++) {
+    if (inOrderLeft[i] !== inOrderRight[inOrderRight.length - i - 1]) return false
+  }
+  return true
+}
+
+const inOrderTraversal = (root: TreeNode | null, result: number[]): void => {
+  if (root === null) return
+  if (!root.left && !root.right) {
+    result.push(root.val)
+    return
+  }
+  if (root.left) inOrderTraversal(root.left, result)
+  else result.push(-Infinity)
+  result.push(root.val)
+  if (root.right) inOrderTraversal(root.right, result)
+  else result.push(-Infinity)
+}
+
+const test: TreeNode = {
+  val: 1,
+  left: {
+    val: 2,
+    left: { val: 2, left: null, right: null },
+    right: null
+  },
+  right: {
+    val: 2,
+    left: { val: 2, left: null, right: null },
+    right: null
+  }
+}
+
+const result = isSymmetric(test)
+console.log(result)
